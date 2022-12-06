b108510c91ad5ddeb63551dbab2da618cb2a14c4
Cengizhan <cengizhandeveci1715@gmail.com>
2022-12-07 00:54:41 +0300

    .
    .

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
-				
-				var TRange=null;
-				function findString (str) {
-				 if (parseInt(navigator.appVersion)<4) return;
-				 var strFound;
-				 if (window.find) {
-				
-				  
-				  strFound=self.find(str);
-				  if (strFound && self.getSelection && !self.getSelection().anchorNode) {
-				   strFound=self.find(str)
-				  }
-				  if (!strFound) {
-				   strFound=self.find(str,0,1)
-				   while (self.find(str,0,1)) continue
-				  }
-				 }
-				 else if (navigator.appName.indexOf("Microsoft")!=-1) {
-				
-			
-				
-				  if (TRange!=null) {
-				   TRange.collapse(false)
-				   strFound=TRange.findText(str)
-				   if (strFound) TRange.select()
-				  }
-				  if (TRange==null || strFound==0) {
-				   TRange=self.document.body.createTextRange()
-				   strFound=TRange.findText(str)
-				   if (strFound) TRange.select()
-				  }
-				 }
-				 else if (navigator.appName=="Opera") {
-				  alert ("Opera browsers not supported, sorry...")
-				  return;
-				 }
-				 if (!strFound) alert ("This word '"+str+"' not found!")
-				 return;
-				}
-				
+
+var TRange = null;
+function findString(str) {
+	if (parseInt(navigator.appVersion) < 4) return;
+	var strFound;
+	if (window.find) {
+
+
+		strFound = self.find(str);
+		if (strFound && self.getSelection && !self.getSelection().anchorNode) {
+			strFound = self.find(str)
+		}
+		if (!strFound) {
+			strFound = self.find(str, 0, 1)
+			while (self.find(str, 0, 1)) continue
+		}
+	}
+	else if (navigator.appName.indexOf("Microsoft") != -1) {
 
 
 
-
-
-                
-	$(".change").on("click", function() {
-        if( $("body").hasClass("dark")) {
-            $("body").removeClass("dark");
-            $(".change").text("🌙");
-        } else {
-            $("body").addClass("dark");
-            $(".change").text("☀️");
-        }
-    });
+		if (TRange != null) {
+			TRange.collapse(false)
+			strFound = TRange.findText(str)
+			if (strFound) TRange.select()
+		}
+		if (TRange == null || strFound == 0) {
+			TRange = self.document.body.createTextRange()
+			strFound = TRange.findText(str)
+			if (strFound) TRange.select()
+		}
+	}
+	else if (navigator.appName == "Opera") {
+		alert("Opera browsers not supported, sorry...")
+		return;
+	}
+	if (!strFound) alert("This word '" + str + "' not found!")
+	return;
+}
 
 
 
 
 
 
-    
+
+$(".change").on("click", function () {
+	if ($("body").hasClass("dark")) {
+		$("body").removeClass("dark");
+		$(".change").text("🌙");
+	} else {
+		$("body").addClass("dark");
+		$(".change").text("☀️");
+	}
+});
+
+
+
+
+
+
