2d459424bf8770b2d35c23d55aeb8aa231560a69
Cengizhan <cengizhandeveci1715@gmail.com>
2022-12-14 02:18:40 +0300

    .
    .

--- script.js
+++ script.js
@@ -56,6 +56,13 @@ $(".change").on("click", function () {
 
 
 
-
-
-
+function openNav() {
+	document.getElementById("mySidebar").style.width = "200px";
+	document.getElementById("main").style.marginLeft = "200px";
+  }
+  
+ 
+  function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+  }
